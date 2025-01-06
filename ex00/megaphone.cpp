8d086971a7099381fc3fc2e01288128968b3f89d
Gaelle Ozon <gozon@MADE-f0Br8s12.clusters.42paris.fr>
2025-01-06 13:08:32 +0100

    Do ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 10:56:27 by gozon             #+#    #+#             */
+/*   Updated: 2025/01/06 13:07:55 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j] != 0; j++)
+				std::cout << (char)std::toupper(av[i][j]);
+		}
+	}
+	std::cout << "\n";
+	return (0);
+}
