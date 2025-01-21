3e9eab85709d598b147997c131d819a85bbd06bb
Gaëlle Ozon <gozon@student.42.fr>
2025-01-21 14:19:34 +0100

    Improve add function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,13 +6,11 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 11:28:11 by gozon             #+#    #+#             */
-/*   Updated: 2025/01/21 13:54:28 by gozon            ###   ########.fr       */
+/*   Updated: 2025/01/21 14:05:09 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
-#include "Contact.hpp"
-#include <iostream>
+#include "awesome_phonebook.hpp"
 
 std::string inputCommand(void){
 	std::string input;
@@ -25,36 +23,6 @@ std::string inputCommand(void){
 		input = "";
 	}
 	return (input);
-}
-
-void inputContact(Phonebook *phonebook){
-	std::string input;
-	Contact contact;
-
-	std::cout << "Input your contact informations" << std::endl;
-
-	std::cout << "First Name: ";
-	std::cin >> input;
-	contact.setFirstName(input);
-
-	std::cout << "Last Name: ";
-	std::cin >> input;
-	contact.setLastName(input);
-
-	std::cout << "Nickname: ";
-	std::cin >> input;
-	contact.setNickname(input);
-
-	std::cout << "Phone Number: ";
-	std::cin >> input;
-	contact.setPhoneNumber(input);
-
-	std::cout << "Darkest Secret: ";
-	std::cin >> input;
-	contact.setDarkestSecret(input);
-
-	(*phonebook).addContact(contact);
-
 }
 
 void executeCommand(const std::string command, Phonebook *phoneBook){
